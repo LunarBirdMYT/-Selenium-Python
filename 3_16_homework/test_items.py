474dc4a321c34f640671f9305b713bd5095de423
LunarBirdMYT <pav.mgk50@gmail.com>
2021-09-19 17:05:27 +0300

    Поправил имя функции в домашнем задании

--- 3_16_homework/test_items.py
+++ 3_16_homework/test_items.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket(browser):
 
     browser.get(link)
     assert browser.find_elements_by_css_selector("button.btn.btn-lg.btn-primary"), 'No such element! Please, find your error in code :)'
